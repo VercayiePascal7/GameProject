b7d21a642c591a75777064f958b4566c876fd2d4
Admin <p.vrcy07@gmail.com>
2026-04-30 13:09:47 +0200

    feat: added test programme for Item(closes #17)

---
+++ src/test/java/org/example/TestItem.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestItem {
+
+    @Test
+    public void testItemName() {
+        Item item = new Item("Karte", "Eine alte Karte.");
+
+        assertEquals("karte", item.getName());
+    }
+
+    @Test
+    public void testItemDisplayName() {
+        Item item = new Item("karte", "Eine alte Karte.");
+
+        assertEquals("Karte", item.getDisplayName());
+    }
+
+    @Test
+    public void testItemDescription() {
+        Item item = new Item("schluessel", "Ein rostiger Schluessel.");
+
+        assertEquals("Ein rostiger Schluessel.", item.getDescription());
+    }
+}
